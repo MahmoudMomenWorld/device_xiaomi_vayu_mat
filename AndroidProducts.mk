8030b191cafcfc4f75be888bc61353def447cee6
Mo'men Abdulrazek Ali <100705049+momenabdulrazek@users.noreply.github.com>
2024-03-09 02:25:46 +0200

    Rearrange

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,6 +8,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_vayu.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_vayu-eng \
     lineage_vayu-userdebug \
-    lineage_vayu-user
+    lineage_vayu-user \
+    lineage_vayu-eng
